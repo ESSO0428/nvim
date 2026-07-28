238d726a7ee74f98cad5a8c555fe197d9c478ae1
ESSO0428 <esso0428@gmail.com>
2026-07-29 03:20:47 +0800

    refactor(lsp): remove custom Marksman root detection

--- after/lsp/marksman.lua
+++ after/lsp/marksman.lua
@@ -1,8 +1,3 @@
-local helpers = require("user.lsp.helpers")
-
 return {
   capabilities = Nvim.builtin.lsp.get_capabilities(),
-  root_dir = helpers.marksman_root_dir,
-  root_markers = helpers.project_root_markers(),
-  single_file_support = true,
 }
--- lua/user/lsp/helpers.lua
+++ lua/user/lsp/helpers.lua
@@ -22,57 +22,8 @@ function M.get_root(bufnr, markers)
   return vim.fs.root(bufnr, markers)
 end
 
-function M.buffer_dir(bufnr)
-  local fname = vim.api.nvim_buf_get_name(bufnr)
-  if fname == nil or fname == "" then
-    return vim.uv.cwd()
-  end
-
-  return vim.fs.dirname(fname)
-end
-
 function M.find_git_ancestor_or_root(bufnr, markers)
   return M.get_root(bufnr, { ".git" }) or M.get_root(bufnr, markers)
-end
-
-function M.project_root_markers()
-  return {
-    ".git",
-    "_darcs",
-    ".hg",
-    ".bzr",
-    ".svn",
-    "Makefile",
-    "package.json",
-    "package-lock.json",
-    "pnpm-lock.yaml",
-    "yarn.lock",
-    "pyproject.toml",
-    "setup.py",
-    "setup.cfg",
-    "requirements.txt",
-    "Pipfile",
-    "Cargo.toml",
-    "go.mod",
-    "composer.json",
-    "pom.xml",
-    "mix.exs",
-    "mkdocs.yml",
-    "mkdocs.yaml",
-  }
-end
-
-function M.project_root_or_buffer_dir(bufnr, extra_markers)
-  local markers = M.project_root_markers()
-  if extra_markers and #extra_markers > 0 then
-    vim.list_extend(markers, extra_markers)
-  end
-
-  return M.get_root(bufnr, markers) or M.buffer_dir(bufnr)
-end
-
-function M.marksman_root_dir(bufnr, on_dir)
-  on_dir(M.project_root_or_buffer_dir(bufnr))
 end
 
 function M.python_root_dir(bufnr, on_dir)
