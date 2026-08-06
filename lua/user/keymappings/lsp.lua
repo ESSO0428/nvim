194e60bdbd6f71d82741733831ed6128781732e3
ESSO0428 <esso0428@gmail.com>
2026-08-06 14:22:34 +0800

    fix(lsp): migrate diagnostic navigation to vim.diagnostic.jump
    Replace the deprecated diagnostic goto_prev() and goto_next() APIs with
    vim.diagnostic.jump(), while preserving the diagnostic float shown after
    each jump.

--- lua/user/keymappings/lsp.lua
+++ lua/user/keymappings/lsp.lua
@@ -30,7 +30,7 @@ local function execute_check_action_toggle_on_filetype()
   end
 end
 
-Nvim.keys.normal_mode["gs"] = { execute_check_action_toggle_on_filetype, { desc = "Toggle checkbox or Antovim" } }
+Nvim.keys.normal_mode["gs"]            = { execute_check_action_toggle_on_filetype, { desc = "Toggle checkbox or Antovim" } }
 Nvim.keys.normal_mode["gh"]            = { "<cmd>lua vim.lsp.buf.hover({ border = 'rounded' })<cr>",
   { desc = "LSP: documentation hover" } }
 Nvim.keys.normal_mode["gm"]            = { "<cmd>lua require('lsp_signature').toggle_float_win()<cr>",
@@ -47,39 +47,41 @@ Nvim.keys.normal_mode["<leader><a-o>"] = {
   { desc = "LSP: [G]oto [D]efinition (preview)" },
 }
 
-Nvim.keys.normal_mode["<leader>ui"] = { "<cmd>LspInfo<cr>", { desc = "LSP: Info" } }
-Nvim.keys.normal_mode["<leader>uI"] = { "<cmd>Mason<cr>", { desc = "LSP: Mason" } }
-Nvim.keys.normal_mode["<leader>ud"] = {
+Nvim.keys.normal_mode["<leader>ui"]    = { "<cmd>LspInfo<cr>", { desc = "LSP: Info" } }
+Nvim.keys.normal_mode["<leader>uI"]    = { "<cmd>Mason<cr>", { desc = "LSP: Mason" } }
+Nvim.keys.normal_mode["<leader>ud"]    = {
   "<cmd>Telescope diagnostics bufnr=0 theme=get_ivy<cr>",
   { desc = "LSP: Buffer Diagnostics" },
 }
-Nvim.keys.normal_mode["<leader>ue"] = { "<cmd>Telescope quickfix<cr>", { desc = "LSP: Quickfix" } }
-Nvim.keys.normal_mode["<leader>ul"] = {
+Nvim.keys.normal_mode["<leader>ue"]    = { "<cmd>Telescope quickfix<cr>", { desc = "LSP: Quickfix" } }
+Nvim.keys.normal_mode["<leader>ul"]    = {
   "<cmd>lua vim.lsp.codelens.run()<cr>",
   { desc = "LSP: CodeLens Action" },
 }
-Nvim.keys.normal_mode["<leader>uq"] = {
+Nvim.keys.normal_mode["<leader>uq"]    = {
   "<cmd>lua vim.diagnostic.setloclist()<cr>",
   { desc = "LSP: Diagnostics to Location List" },
 }
-Nvim.keys.normal_mode["<leader>us"] = {
+Nvim.keys.normal_mode["<leader>us"]    = {
   "<cmd>Telescope lsp_document_symbols<cr>",
   { desc = "LSP: Document Symbols" },
 }
-Nvim.keys.normal_mode["<leader>uS"] = {
+Nvim.keys.normal_mode["<leader>uS"]    = {
   "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
   { desc = "LSP: Workspace Symbols" },
 }
-Nvim.keys.normal_mode['gr']         = { "<cmd>Telescope lsp_references<cr>", { desc = "lsp_references" } }
-Nvim.keys.normal_mode['<leader>v']  = { "<cmd>Telescope lsp_document_symbols<cr>", { desc = "lsp_document_symbols" } }
-Nvim.keys.normal_mode['<leader>V']  = { "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
-  desc = "lsp_dynamic_workspace_symbols" }
+Nvim.keys.normal_mode['gr']            = { "<cmd>Telescope lsp_references<cr>", { desc = "lsp_references" } }
+Nvim.keys.normal_mode['<leader>v']     = { "<cmd>Telescope lsp_document_symbols<cr>", { desc = "lsp_document_symbols" } }
+Nvim.keys.normal_mode['<leader>V']     = {
+  "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
+  desc = "lsp_dynamic_workspace_symbols"
+}
 
-Nvim.keys.normal_mode["<c-t>"] = { "<cmd>Outline!<cr>", { desc = "LSP: Outline Toggle" } }
+Nvim.keys.normal_mode["<c-t>"]         = { "<cmd>Outline!<cr>", { desc = "LSP: Outline Toggle" } }
 Nvim.keys.normal_mode["<leader><c-t>"] = { "<cmd>OutlineFocusOutline<cr>", { desc = "LSP: Outline Focus" } }
-Nvim.keys.normal_mode["<leader>I"] = { "<cmd>wincmd W<cr>", { desc = "LSP: Other Window" } }
-Nvim.keys.normal_mode["<leader>K"] = { "<cmd>wincmd w<cr>", { desc = "LSP: Next Window" } }
-Nvim.keys.normal_mode["gl"] = { function()
+Nvim.keys.normal_mode["<leader>I"]     = { "<cmd>wincmd W<cr>", { desc = "LSP: Other Window" } }
+Nvim.keys.normal_mode["<leader>K"]     = { "<cmd>wincmd w<cr>", { desc = "LSP: Next Window" } }
+Nvim.keys.normal_mode["gl"]            = { function()
   local float = vim.diagnostic.config().float
 
   if float then
@@ -89,8 +91,12 @@ Nvim.keys.normal_mode["gl"] = { function()
     vim.diagnostic.open_float(config)
   end
 end, { desc = "LSP: Show line diagnostics" } }
-Nvim.keys.normal_mode["<"] = { vim.diagnostic.goto_prev, { desc = "goto prev diagnostic", nowait = true } }
-Nvim.keys.normal_mode[">"] = { vim.diagnostic.goto_next, { desc = "goto next diagnostic", nowait = true } }
+
+local function diagnostic_jump(count)
+  vim.diagnostic.jump({ count = count, float = true })
+end
+Nvim.keys.normal_mode["<"]          = { function() diagnostic_jump(-1) end, { desc = "goto prev diagnostic", nowait = true } }
+Nvim.keys.normal_mode[">"]          = { function() diagnostic_jump(1) end, { desc = "goto next diagnostic", nowait = true } }
 Nvim.keys.normal_mode["<leader>u="] = {
   function()
     require("conform").format { async = true, lsp_format = "fallback" }
@@ -103,4 +109,4 @@ Nvim.keys.normal_mode["<leader>uf"] = {
   end,
   { desc = "LSP: Format", nowait = true },
 }
-Nvim.keys.normal_mode["<leader>="] = { vim.lsp.buf.format, { desc = "Lsp Buffer Format", nowait = true } }
+Nvim.keys.normal_mode["<leader>="]  = { vim.lsp.buf.format, { desc = "Lsp Buffer Format", nowait = true } }
